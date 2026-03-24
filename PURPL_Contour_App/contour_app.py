fb3fe330893bcce45a3f895d6089bed1a4c34afa
devinmorris15 <devinmorrisjr06@gmail.com>
2026-03-24 13:57:46 -0400

    Update contour_app.py

--- PURPL_Contour_App/contour_app.py
+++ PURPL_Contour_App/contour_app.py
@@ -257,12 +257,12 @@ with st.sidebar:
 		with col1:
 			ox = st.selectbox("Oxidizer", ["LOX", "N2O"], index=0) #Choose Oxidizer Select Box
 		with col2:
-			fuel = st.selectbox("Fuel", ["RP_1", "LH2", "CH4", "IPA"], index=0) #Choose Fuel Select Box
+			fuel = st.selectbox("Fuel", ["RP_1", "LH2", "CH4", "Isopropanol"], index=0) #Choose Fuel Select Box
 	else:
 		with col1:
 			ox = st.selectbox("Oxidizer", ["90_H2O2", "98_H2O2","LOX", "N2O"], index=0) #Choose Oxidizer Select Box
 		with col2:
-			fuel = st.selectbox("Fuel", ["RP_1", "GH2", "IPA"], index=0) #Choose Fuel Select Box
+			fuel = st.selectbox("Fuel", ["RP_1", "GH2", "Isopropanol"], index=0) #Choose Fuel Select Box
 
 	with col3:
 		of_ratio = st.number_input(label="O/F Ratio", min_value=0.0, max_value=100.0, value=None, step=0.1)#Input Box for O/F Ratio
