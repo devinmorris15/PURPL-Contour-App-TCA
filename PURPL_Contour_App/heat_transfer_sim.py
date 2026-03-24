49b94a935f44d88b8e289954853d82502958d77f
devinmorris15 <devinmorrisjr06@gmail.com>
2026-03-24 15:37:51 -0400

    Update heat_transfer_sim.py

--- PURPL_Contour_App/heat_transfer_sim.py
+++ PURPL_Contour_App/heat_transfer_sim.py
@@ -132,25 +132,25 @@ def run_simulation(mat, geom, cea_cfg, solv_cfg, env_cfg):
     Pr   = props["Pr"]
     Ma   = props["Ma_ref"]
 
-    print(
-        f"  Station : {station}\n"
-        f"  T_g     : {T_g:.1f} K  ({K_to_F(T_g):.1f} °F)\n"
-        f"  gamma   : {gam:.4f}\n"
-        f"  cp_g    : {cp_g:.1f} J/(kg·K)\n"
-        f"  mu      : {mu:.3e} Pa·s\n"
-        f"  Pr      : {Pr:.4f}\n"
-        f"  Ma_ref  : {Ma:.2f}"
-    )
+    #print(
+    #    f"  Station : {station}\n"
+    #    f"  T_g     : {T_g:.1f} K  ({K_to_F(T_g):.1f} °F)\n"
+    #    f"  gamma   : {gam:.4f}\n"
+    #    f"  cp_g    : {cp_g:.1f} J/(kg·K)\n"
+    #    f"  mu      : {mu:.3e} Pa·s\n"
+    #    f"  Pr      : {Pr:.4f}\n"
+    #    f"  Ma_ref  : {Ma:.2f}"
+    #)
 
-    st.code(f"""
-        Station : {station}
-        T_g     : {T_g:.1f} K  ({K_to_F(T_g):.1f} °F)
-        gamma   : {gam:.4f}
-        cp_g    : {cp_g:.1f} J/(kg·K)
-        mu      : {mu:.3e} Pa·s
-        Pr      : {Pr:.4f}
-        Ma_ref  : {Ma:.2f}
-        """)
+    #st.code(f"""
+    #    Station : {station}
+    #    T_g     : {T_g:.1f} K  ({K_to_F(T_g):.1f} °F)
+    #    gamma   : {gam:.4f}
+    #    cp_g    : {cp_g:.1f} J/(kg·K)
+    #    mu      : {mu:.3e} Pa·s
+    #    Pr      : {Pr:.4f}
+    #    Ma_ref  : {Ma:.2f}
+    #    """)
 
     Pc_Pa = psi_to_Pa(cea_cfg["Pc_chamber_psia"])
     Cstar = float(cea.get_Cstar(Pc=cea_cfg["Pc_chamber_psia"], MR=cea_cfg["MR"])) * 0.3048
