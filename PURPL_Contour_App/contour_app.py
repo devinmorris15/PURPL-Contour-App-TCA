2d459b9e304eb90c9f3be6a4a24ce654639a1bcb
devinmorris15 <devinmorrisjr06@gmail.com>
2026-04-27 20:44:18 -0400

    Update contour_app.py

--- PURPL_Contour_App/contour_app.py
+++ PURPL_Contour_App/contour_app.py
@@ -271,7 +271,7 @@ st.markdown("""
     <style>
         .block-container {
             padding-top: 0rem;
-            margin-top: -2rem;
+            margin-top: -5rem;
         }
     </style>
 """, unsafe_allow_html=True)
